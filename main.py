e8db50135f3977089b59a547f5cb53963fc80f92
inikanipp <pengumpulanlomba@gmail.com>
2026-02-25 14:56:57 +0700

    update

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes import file_routes
 
-# is_production = os.getenv("ENV") == "production"
+is_production = os.getenv("ENV") == "production"
 
-# app = FastAPI(
-#     docs_url=None if is_production else "/docs",
-#     redoc_url=None if is_production else "/redoc",
-#     openapi_url=None if is_production else "/openapi.json"
-# )
+app = FastAPI(
+    docs_url=None if is_production else "/docs",
+    redoc_url=None if is_production else "/redoc",
+    openapi_url=None if is_production else "/openapi.json"
+)
 
-app = FastAPI()
+# app = FastAPI()
 
 app.include_router(file_routes.router)
 
